e143931243d51fb50f53131b62a1ef1f531a427c
Dan Finelli <dan.finelli@hotmail.com>
2016-12-19 01:40:37 -0800

    finshed index

--- bog_app/bog_app_3/db/seeds.rb
+++ bog_app/bog_app_3/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Creature.create({ name: "yo mama", description: "yo mamma" })
+Creature.create({ name: "cookie monster", description: "monster that eats cookiessss" })
