d44b198ea1026d4f90fdfd5cb4323611d62b84b3
Dan Finelli <dan.finelli@hotmail.com>
2016-12-18 22:20:07 -0800

    update is creating new character?" / "

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -3,11 +3,9 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
   end
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
-
     if creature.save
-      redirect_to creatures_path
+      redirect_to creature_path(creature)
     end
   end
   def new
@@ -17,5 +15,18 @@ class CreaturesController < ApplicationController
     creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
   end
-
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
 end
