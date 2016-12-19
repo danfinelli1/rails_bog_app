6a14852246b79928d36df41a185da828c4e68f49
Dan Finelli <dan.finelli@hotmail.com>
2016-12-18 23:01:56 -0800

    finished round one"

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -22,6 +22,11 @@ class CreaturesController < ApplicationController
     creature.update_attributes(creature_params)
     redirect_to creature_path(creature)
   end
+  def destroy
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+  end
 private
   def creature_id
     params[:id]
