a6667e00ce4ec0c3bfc3ca15e0380bed80b1aec4
Dan Finelli <dan.finelli@hotmail.com>
2016-12-19 00:54:25 -0800

    actually done. had to fix a few bugs in last commit

--- bog_app_2/app/controllers/creatures_controller.rb
+++ bog_app_2/app/controllers/creatures_controller.rb
@@ -8,8 +8,8 @@ def new
 end
 def create
   @creature = Creature.new(creature_params)
-  if creature.save
-    redirect_to creatures_path(creature)
+  if @creature.save
+    redirect_to creatures_path()
   end
 end
 def show
