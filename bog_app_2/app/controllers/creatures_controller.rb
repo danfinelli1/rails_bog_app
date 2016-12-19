10cc5d9afbb23bd7c1f7cbedf28523862577b344
Dan Finelli <dan.finelli@hotmail.com>
2016-12-19 00:30:07 -0800

    added create

---
+++ bog_app_2/app/controllers/creatures_controller.rb
@@ -0,0 +1,24 @@
+class CreaturesController < ApplicationController
+
+def index
+  @creatures = Creature.all
+end
+def new
+  @creature = Creature.new
+end
+def create
+  @creature = Creature.new(creature_params)
+  if creature.save
+    redirect_to creatures_path(creature)
+  end
+end
+
+private
+def creature_id
+  params[:id]
+end
+def creature_params
+  params.require(:creature).permit(:name, :description)
+end
+
+end
