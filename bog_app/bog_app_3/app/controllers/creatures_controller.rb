1eda5777656701934d5d551e61e807db7e517692
Dan Finelli <dan.finelli@hotmail.com>
2016-12-19 07:32:51 -0800

    finished this run

--- bog_app/bog_app_3/app/controllers/creatures_controller.rb
+++ bog_app/bog_app_3/app/controllers/creatures_controller.rb
@@ -14,6 +14,19 @@ class CreaturesController < ApplicationController
   def show
     @creature = Creature.find_by_id(creature_id)
   end
+  def edit
+    @creature = Creature.find_by_id(creature_id)
+  end
+  def update
+    creature = Creature.find_by_id(creature_id)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+  def destroy
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+  end
 private
   def creature_id
     params[:id]
